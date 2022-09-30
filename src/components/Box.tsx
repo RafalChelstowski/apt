f9c6828b1be4a279979f9f4169e3d1dac1817b63
Rafał Chełstowski <chelstowskirafaldev@gmail.com>
2022-09-30 22:36:43 +0200

    geolocation

--- src/components/Box.tsx
+++ src/components/Box.tsx
@@ -2,14 +2,15 @@ import { useQuery } from '@tanstack/react-query';
 import { getGpsCoordinates } from '~/api/geolocation';
 
 export function Box() {
-  const { data } = useQuery(['geolocation'], () => getGpsCoordinates());
+  const { isLoading, error } = useQuery(['geolocation'], getGpsCoordinates);
 
-  console.log(data);
+  const loadingColor = isLoading ? 'blue' : 'gray';
+  const statusColor = error ? 'red' : 'green';
 
   return (
     <mesh>
       <boxGeometry args={[1, 1, 1]} />
-      <meshStandardMaterial color='orange' />
+      <meshStandardMaterial color={isLoading ? loadingColor : statusColor} />
     </mesh>
   );
 }
